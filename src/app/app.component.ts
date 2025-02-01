712ba3b5daf55d0480fe9acbdd33e5d54ebca013
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2025-02-01 19:59:04 -0300

    pasando header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PacientesComponent } from './components/pacientes/pacientes.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PacientesComponent, CommonModule],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent , PacientesComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
