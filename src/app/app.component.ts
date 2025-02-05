fed8dfd4c631ff14fb40862b6a41ef98c35e8124
CeciliaMon <dreamynylle17@gmail.com>
2025-02-05 05:47:44 -0300

    FormPaciente e InformacionPaciente agregados con sus rutas en el nav, diseño de la tabla de Paciente agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from "./components/home/home.component";
+import { FormPacienteComponent } from "./components/form-paciente/form-paciente.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PacientesComponent, CommonModule, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PacientesComponent, CommonModule, HomeComponent, FormPacienteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FormPacienteComponent } from './components/form-paciente/form-paciente.component';
+import { HomeComponent } from './components/home/home.component';
+import { PacientesComponent } from './components/pacientes/pacientes.component';
+import { InformacionPacienteComponent } from './components/informacion-paciente/informacion-paciente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'formPaciente', component: FormPacienteComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'pacientes', component: PacientesComponent },
+    { path: 'informacionPacientes', component: InformacionPacienteComponent }
+];
