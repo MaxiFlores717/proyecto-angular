030ea983abe0a56430fa77dab012376efd2ccc70
CeciliaMon <dreamynylle17@gmail.com>
2025-02-05 01:38:26 -0300

    Diseño entre el nav y el main de la página corregido y arreglo de las imágenes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  imageVista1: string = "assets/images/vista1.jpg";
+  imageVista2: string = "assets/images/vista2.jpg";
+  imageVista3: string = "assets/images/vista3.jpg";
+  imageHorarios: string = "assets/images/Horarios.heic";
+  imageDireccion: string = "assets/images/Direccion-clinica.png";
+  imageLimpieza: string = "assets/images/Limpieza-dental.jpg";
+  imageOrtodoncia: string = "assets/images/Ortodoncia2.jpg";
+  imageEndodoncia: string = "assets/images/Endodoncia.jpg";
+  imageImplante: string = "assets/images/Implante-dental.jpg";
+  imageDecodificacion: string = "assets/images/Decodificacion-dental.heic";
 
+  imageOdontoPediatria: string = "assets/images/Odontopediatria.heic";
+  imageBlanqueamiento: string = "assets/images/Blanqueamiento.heic";
+  imageCirugia: string = "assets/images/Cirugia-dental.heic";
+  imageEstetica: string = "assets/images/Estetica.jpg";
+  
 }
--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+    imageLogo: string = "assets/images/icono2.jpg";
 }
