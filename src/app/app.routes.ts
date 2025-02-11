df2a4eda69161d85cb5ee061df8f49b142eded56
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2025-02-11 19:58:48 -0300

    ordenando carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { FormPacienteComponent } from './components/pacientes/form-paciente/form-paciente.component';
 import { HomeComponent } from './components/home/home.component';
 import { PacientesComponent } from './components/pacientes/pacientes.component';
-import { InformacionPacienteComponent } from './components/informacion-paciente/informacion-paciente.component';
+
 import { DataTableComponent } from './components/pacientes/data-table/data-table.component';
+import { InformacionPacienteComponent } from './components/pacientes/informacion-paciente/informacion-paciente.component';
 
 export const routes: Routes = [
     { path: 'formPaciente', component: PacientesComponent },
